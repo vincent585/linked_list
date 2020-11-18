c491084110bb4f509ae7b82cefa1e25ced539b35
Vincent Klimjack <vklimjack@gmail.com>
2020-11-18 11:55:59 +0000

    added append method

--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,18 @@
+require_relative 'node'
+
 class LinkedList
-  attr_accessor :name
+  attr_accessor :name, :head, :tail, :size
   def initialize
     @head = nil
     @tail = nil
+    @size = 0
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+    @head.nil? ? @head = new_node : @tail.next_node = new_node
+    @tail = new_node
+    @size += 1
   end
 end
+
