209c34d120346227fcad5166561547a5676c3e78
Vincent Klimjack <vklimjack@gmail.com>
2020-11-18 12:39:06 +0000

    prepend and at methods added

--- linked_list.rb
+++ linked_list.rb
@@ -14,5 +14,41 @@ class LinkedList
     @tail = new_node
     @size += 1
   end
+
+  def prepend(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      new_node.next_node = @head
+      @head = new_node
+    end
+    @size += 1
+  end
+
+  def at(index)
+    return 'Invalid index' unless index.is_a?(Integer)
+    return 'nil' if index > size - 1
+
+    current_node = head
+    index.times do
+      current_node = current_node.next_node
+    end
+    current_node
+  end
+
+  def pop
+    return 'nil' if size.zero?
+
+
+  end
 end
 
+list = LinkedList.new
+list.append(42)
+list.append(100)
+list.prepend(12)
+p list.at(0)
+p list.at(2)
+p list.at(3)
